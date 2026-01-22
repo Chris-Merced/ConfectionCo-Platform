0074359e93c24ead9e364e4be3c9122764ce6010
Chris-Merced <christopher.r.merced@gmail.com>
2026-01-22 15:33:35 -0600

    Initialize project with spring boot web

---
+++ confectionco/src/main/java/com/chrismerced/projects/confectionco/ConfectioncoApplication.java
@@ -0,0 +1,13 @@
+package com.chrismerced.projects.confectionco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfectioncoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfectioncoApplication.class, args);
+	}
+
+}
