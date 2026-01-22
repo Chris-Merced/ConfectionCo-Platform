1361f0c5f037b3a6b25c209d0d996ec204db0fc7
Chris-Merced <christopher.r.merced@gmail.com>
2026-01-22 15:33:35 -0600

    Initialize project with spring boot web

---
+++ confectionco/src/test/java/com/chrismerced/projects/confectionco/ConfectioncoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrismerced.projects.confectionco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfectioncoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
