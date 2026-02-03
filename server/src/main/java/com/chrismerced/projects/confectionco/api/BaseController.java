a506161a284a35f89bb872a86c62d46bd673a83e
Chris-Merced <christopher.r.merced@gmail.com>
2026-02-03 17:09:26 -0600

    Add TODOs

--- server/src/main/java/com/chrismerced/projects/confectionco/api/BaseController.java
+++ server/src/main/java/com/chrismerced/projects/confectionco/api/BaseController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+//TODO: Modify CORS to ENV variables between prod and dev
 @CrossOrigin(origins = "http://localhost:5173")
 
 @RestController
