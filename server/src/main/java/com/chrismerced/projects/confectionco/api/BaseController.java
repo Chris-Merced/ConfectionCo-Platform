813b5ece5f25373cfd620e25c8248cafa14ba160
Chris-Merced <christopher.r.merced@gmail.com>
2026-02-03 17:04:35 -0600

    Connect Front and Back End for Development
    Create CORS settings in BaseController
    Modify return on /api/base to valid JSON
    
    Create fetch from react function to backend
    Display backend data on frontend in React comp

--- server/src/main/java/com/chrismerced/projects/confectionco/api/BaseController.java
+++ server/src/main/java/com/chrismerced/projects/confectionco/api/BaseController.java
@@ -1,13 +1,18 @@
 package com.chrismerced.projects.confectionco.api;
 
+import java.util.Map;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+@CrossOrigin(origins = "http://localhost:5173")
 
 @RestController
 public class BaseController {
     
     @GetMapping("/api/base")
-    public String base(){
-        return "ok";
+    public Map<String, String> base(){
+        return Map.of("status", "ok");
     }
 }
