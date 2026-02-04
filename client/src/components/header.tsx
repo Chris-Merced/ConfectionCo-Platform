961035f3882f6d95ce5542a3455e14531c445b85
Chris-Merced <christopher.r.merced@gmail.com>
2026-02-04 14:11:54 -0600

    Add header component
    Ensure component and styling is properly setup

---
+++ client/src/components/header.tsx
@@ -0,0 +1,10 @@
+import type { ReactElement } from "react";
+import "../styles.css";
+
+export function Header(): ReactElement {
+    return (
+        <>
+            <div className="Header">Wow look at me</div>
+        </>
+    )
+}
