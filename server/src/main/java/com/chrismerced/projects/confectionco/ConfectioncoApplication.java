bbeba3b8f26841cc8da88be48e6a506e9bca9e35
Chris-Merced <christopher.r.merced@gmail.com>
2026-02-19 13:40:21 -0600

    Change website layout in comments
    Context functionality added
    Further clarification of items structure
    Further clarificaiton on cart limits

--- server/src/main/java/com/chrismerced/projects/confectionco/ConfectioncoApplication.java
+++ server/src/main/java/com/chrismerced/projects/confectionco/ConfectioncoApplication.java
@@ -17,11 +17,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // Need to figure out texting system
 // Create a high level layout for the website (in total what pages need to be made)
 //  - context wrap the cart from local storage 
-//		- Main page for ordering
-//   		- - disappearing header
-//   		- - - main content that shows menu with clickthrough to ordering
-//   		- - - set cart to local storage which will be an array of items with the same order number
-
+//		- Header containing cart info
+//			- Main page for ordering
+//   			- - - main content that shows menu with clickthrough to ordering
+//   			- - - set cart to local storage which will be an array of items with the same order number
+//				- - - - Each item will contain full information on order with description and inspo
+// 				- - - - Orders cannot be for two seperate locations or events
+// 			- Cart Page for completing order
 
 @SpringBootApplication
 public class ConfectioncoApplication {
