551733c28f564b54136974b8b54e60a98e742fc3
Chris-Merced <christopher.r.merced@gmail.com>
2026-04-01 14:59:42 -0500

    Improve workflow and ensure api responses work

--- client/src/components/main.tsx
+++ client/src/components/main.tsx
@@ -1,10 +1,42 @@
-import type { ReactElement } from "react";
+import { useEffect, useState, type ReactElement } from "react";
 import "../styles.css";
 
 export function Main(): ReactElement {
+
+    type apiResponse = { status: string };
+
+    const [newVal, useNewVal] = useState<apiResponse>()
+    const [status, useStatus] = useState<string>('')
+
+    useEffect(() => {
+        async function getSomething() {
+            const res = await fetch('http://localhost:8080/api/base')
+            const data = await res.json();
+            useNewVal(data)
+        }
+
+        getSomething();
+    }, [])
+
+    console.log("here is your value: ")
+    console.log(newVal)
+
+    useEffect(() => {
+        if (newVal) {
+            console.log("achieved")
+            console.log(newVal.status)
+            const statusVal = newVal.status
+            console.log(statusVal)
+            useStatus(statusVal)
+        }
+    }, [newVal])
+
     return (
         <>
             <div className="main">We have made it to main display</div>
+            <></>
+
+            <>Status: {status}</>
         </>
     )
 }
