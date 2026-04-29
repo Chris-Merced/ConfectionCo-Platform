c916dcf546f467c3e291197ce829cb060fd0c990
Chris-Merced <christopher.r.merced@gmail.com>
2026-04-29 00:13:25 -0500

    Moved TODOs with relevant info to TODO.txt

--- server/src/main/java/com/chrismerced/projects/confectionco/ConfectioncoApplication.java
+++ server/src/main/java/com/chrismerced/projects/confectionco/ConfectioncoApplication.java
@@ -9,8 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // 	- - Set up email service to proof of concept level -
 //  - - - Set up Confection.Co850 Domain
 //  - - - - Get email registered
-//  - Need to create outline for front end on where insta feed is displayed
-//  - - check how easily accessible the insta api is if at all
 @SpringBootApplication
 public class ConfectioncoApplication {
 
