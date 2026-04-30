2368f8532c92fcccf77cbe2bcc2d2dba2fe2bed9
Chris-Merced <christopher.r.merced@gmail.com>
2026-04-30 00:30:17 -0500

    Cleanup imports for EmailService.java

--- server/src/main/java/com/chrismerced/projects/confectionco/services/EmailService.java
+++ server/src/main/java/com/chrismerced/projects/confectionco/services/EmailService.java
@@ -1,7 +1,5 @@
 package com.chrismerced.projects.confectionco.services;
 
-import org.springframework.stereotype.Service;
-
 public interface EmailService {
     void sendEmail();
     void sendReceipt();
