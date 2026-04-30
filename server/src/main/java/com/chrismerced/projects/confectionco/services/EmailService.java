08b52f596a51fbc226052d067763866a492f7973
Chris-Merced <christopher.r.merced@gmail.com>
2026-04-30 00:29:26 -0500

    Implement dependency inversion with email/text interfaces

--- server/src/main/java/com/chrismerced/projects/confectionco/services/EmailService.java
+++ server/src/main/java/com/chrismerced/projects/confectionco/services/EmailService.java
@@ -2,16 +2,8 @@ package com.chrismerced.projects.confectionco.services;
 
 import org.springframework.stereotype.Service;
 
-@Service
-public class EmailService {
-   
-    Email email;
-
-    EmailService(Email email){
-        this.email = email;
-    }
-
-    public void sendEmail(){
-        
-    }
+public interface EmailService {
+    void sendEmail();
+    void sendReceipt();
 }
+
---
+++ server/src/main/java/com/chrismerced/projects/confectionco/services/TwilioService.java
@@ -0,0 +1,11 @@
+package com.chrismerced.projects.confectionco.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TwilioService implements TextingService {
+
+    public void sendText() {
+        System.out.println("lmao");
+    }
+}
