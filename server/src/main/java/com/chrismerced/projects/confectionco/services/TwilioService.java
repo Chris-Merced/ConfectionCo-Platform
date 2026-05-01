e031edfb469692c2eaaafe32e68d176ac2436fc5
Chris-Merced <christopher.r.merced@gmail.com>
2026-05-01 16:22:50 -0500

    Set up barebones Twilio Service with api call

--- server/src/main/java/com/chrismerced/projects/confectionco/services/TwilioService.java
+++ server/src/main/java/com/chrismerced/projects/confectionco/services/TwilioService.java
@@ -1,11 +1,27 @@
 package com.chrismerced.projects.confectionco.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
 
 @Service
 public class TwilioService implements TextingService {
 
+    @Value("${TWILIO_ACCOUNT_SID}")
+    private String accountSID;
+
+    @Value("${TWILIO_AUTH_TOKEN}")
+    private String authToken;
+
     public void sendText() {
-        System.out.println("lmao");
+      System.out.println("Invoked sendText");
+       Twilio.init(accountSID,authToken);
+    Message message = Message.creator(
+      new com.twilio.type.PhoneNumber("+18777804236"),
+      "MG8e2f4b9c6edc84900475493a6d36573e",
+      "Message from application").create();
+    System.out.println(message.getSid());
     }
 }
