56638c01ba56aef7890ccac62130ba2543d3b2dc
Chris-Merced <christopher.r.merced@gmail.com>
2026-05-01 16:22:30 -0500

    Set up barebones Twilio Service

--- client/src/components/main.tsx
+++ client/src/components/main.tsx
@@ -8,6 +8,7 @@ export function Main(): ReactElement {
     const [newVal, useNewVal] = useState<apiResponse>()
     const [status, useStatus] = useState<boolean | null>(null)
 
+    //Send Email Receipt
     useEffect(() => {
         async function getSomething() {
             try {
@@ -16,12 +17,35 @@ export function Main(): ReactElement {
                 useNewVal(data)
             } catch (error) {
                 console.log("whoops teehee")
-                useNewVal({ success : false })
+                useNewVal({ success: false })
             }
         }
 
         getSomething();
     }, [])
+
+    //Send Text Message
+    useEffect(() => {
+        async function sendText() {
+            try {
+                
+                    console.log("starting the processs of text sending")
+                    const res = await fetch("http://localhost:8080/api/base")
+                    const data = await res.json()
+
+                    console.log(data)
+                }
+              catch (err) {
+                console.log("Whoops text error" + err)
+            
+            
+            }
+           
+        }
+
+        sendText();
+    }, [])
+
 
     console.log("here is your value: ")
     console.log(newVal)
