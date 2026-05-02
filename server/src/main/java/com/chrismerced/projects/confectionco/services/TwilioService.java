ef442d367d0638c66e6b482e255496c8847fae19
Chris-Merced <christopher.r.merced@gmail.com>
2026-05-01 23:30:32 -0500

    Cleanup formatting in ResendEmailService

--- server/src/main/java/com/chrismerced/projects/confectionco/services/TwilioService.java
+++ server/src/main/java/com/chrismerced/projects/confectionco/services/TwilioService.java
@@ -9,19 +9,21 @@ import com.twilio.rest.api.v2010.account.Message;
 @Service
 public class TwilioService implements TextingService {
 
-    @Value("${TWILIO_ACCOUNT_SID}")
-    private String accountSID;
+  @Value("${TWILIO_ACCOUNT_SID}")
+  private String accountSID;
 
-    @Value("${TWILIO_AUTH_TOKEN}")
-    private String authToken;
+  @Value("${TWILIO_AUTH_TOKEN}")
+  private String authToken;
 
-    public void sendText() {
-      System.out.println("Invoked sendText");
-       Twilio.init(accountSID,authToken);
+  public void sendText() {
+    System.out.println("Invoked sendText");
+
+    Twilio.init(accountSID, authToken);
+
     Message message = Message.creator(
-      new com.twilio.type.PhoneNumber("+18777804236"),
-      "MG8e2f4b9c6edc84900475493a6d36573e",
-      "Message from application").create();
+        new com.twilio.type.PhoneNumber("+18777804236"),
+        "MG8e2f4b9c6edc84900475493a6d36573e",
+        "Message from application").create();
     System.out.println(message.getSid());
-    }
+  }
 }
