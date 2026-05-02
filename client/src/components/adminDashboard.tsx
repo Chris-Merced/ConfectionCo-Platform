1aca3f7bb094ec74d3ef82813c50f90971b5f902
Chris-Merced <christopher.r.merced@gmail.com>
2026-05-02 11:56:14 -0500

    Set up Admin Dashboard route
    Set up lazy loading for adminDashboard
    Modify webpack to handle split chunking
    Modify webpack to handle naming for vendor and chunk files

--- client/src/components/adminDashboard.tsx
+++ client/src/components/adminDashboard.tsx
@@ -1,7 +1,7 @@
 import { type ReactElement } from "react";
 
 
-export function AdminDashboard(): ReactElement{
+export default function AdminDashboard(): ReactElement{
 
     return(<><p>Welcome to the Admin Dashboard</p></>)
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,24 +1,30 @@
 import type { ReactElement } from "react";
 import { createRoot } from "react-dom/client";
-import { Header } from "./components/header";
-import { Main } from "./components/main";
-import { AdminDashboard } from "./components/adminDashboard"
+import  Header  from "./components/header";
+import Main  from "./components/main";
 import {BrowserRouter} from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
+import {lazy, Suspense} from "react";
 import "./styles.css";
 
+const AdminDashboard = lazy((): any=> import("./components/adminDashboard"))
 //TODO: Create Router and import to app function
 // Create EmailService
 // Utilize Resend as an email service
 function App(): ReactElement {
 
+  
+
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<><Header /><Main /></>} />
-          <Route path="/admin" element={<AdminDashboard/>} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<><Header /><Main /></>} />
+            <Route path="/admin" element={<AdminDashboard/>} />
+            <Route path="*" element={<div>Page not found</div>} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </>
   )
