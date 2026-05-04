cfe3ba584ac556de508251f9823edb4b33a09e6e
Chris-Merced <christopher.r.merced@gmail.com>
2026-05-04 15:08:25 -0500

    Change redirect login behavior
    Remove redirect urls
    Allow Auth0 to redirect to root page
    Create AuthWrapper.tsx to check appstate and redirect accordingly

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,42 +1,44 @@
-import type { ReactElement } from "react";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import  Header  from "./components/header";
-import Main  from "./components/main";
-import {BrowserRouter} from "react-router-dom";
-import { Route, Routes } from "react-router-dom";
-import {lazy, Suspense} from "react";
+import type { AppState } from "@auth0/auth0-react";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
+import AuthWrapper from "./components/authWrapper";
+
+import type { ReactElement } from "react";
+import { lazy, StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/header";
+import Main from "./components/main";
 import "./styles.css";
 
-const AdminDashboard = lazy((): any=> import("./components/adminDashboard"))
+const AdminDashboard = lazy((): any => import("./components/adminDashboard"))
 //TODO: Create Router and import to app function
 // Create EmailService
 // Utilize Resend as an email service
 function App(): ReactElement {
 
-  
+  const onRedirectCallback = (appState?: AppState) => {
+    window.location.pathname = appState?.returnTo || "/";
+  };
 
   return (
     <>
       <StrictMode>
-        <Auth0Provider
-                domain="dev-oayu8epftihuelj1.us.auth0.com"
-                clientId="dxRIYZwKz8Zh790ccHzAwctQ3JcqJe1T"
-                authorizationParams={{ redirect_uri: window.location.origin }}
-              >
-        <BrowserRouter>
-          <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-              <Route path="/" element={<><Header /><Main /></>} />
-              
-                <Route path="/admin" element={<AdminDashboard/>} />
-              
-              <Route path="*" element={<div>Page not found</div>} />
-            </Routes>
-          </Suspense>
-        </BrowserRouter>
-      </Auth0Provider>
+          <BrowserRouter>
+                  <AuthWrapper>
+
+            <Suspense fallback={<div>Loading...</div>}>
+              <Routes>
+                <Route path="/" element={<><Header /><Main /></>} />
+
+                <Route path="/admin" element={<AdminDashboard />} />
+
+                <Route path="*" element={<div>Page not found</div>} />
+              </Routes>
+            </Suspense>
+            </AuthWrapper>
+          </BrowserRouter>
+        
       </StrictMode>
     </>
   )
