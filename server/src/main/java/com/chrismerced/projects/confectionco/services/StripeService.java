a682efdc650de2130f867819127d3bd16541358f
Chris-Merced <christopher.r.merced@gmail.com>
2026-05-05 12:51:55 -0500

    Create basic stripe service for downpayment

--- server/src/main/java/com/chrismerced/projects/confectionco/services/StripeService.java
+++ server/src/main/java/com/chrismerced/projects/confectionco/services/StripeService.java
@@ -1,16 +1,50 @@
 package com.chrismerced.projects.confectionco.services;
 
-public class StripeService implements PaymentService{
-    
+import com.stripe.Stripe;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
-    StripeService(){
+@Service
+public class StripeService {
 
-   }
+    @Value("${stripe.api.key}")
+    private String apiKey;
 
-   //Generates Link to be sent through Twilio Service
-   public String generateLink(){
-        return ("Dummy Text");
-   }
+    @PostConstruct
+    public void init() {
+        Stripe.apiKey = apiKey;
+    }
 
-   //Need to add webhook to be used to send messages to Isabel on payment processed
+    public String createDepositCheckout(Long orderId, long amountInCents) throws Exception {
+
+        SessionCreateParams params =
+                SessionCreateParams.builder()
+                        .setMode(SessionCreateParams.Mode.PAYMENT)
+                        .setSuccessUrl("http://localhost:5173/payment-success?orderId=" + orderId)
+                        .setCancelUrl("http://localhost:5173/payment-cancel")
+                        .addLineItem(
+                                SessionCreateParams.LineItem.builder()
+                                        .setQuantity(1L)
+                                        .setPriceData(
+                                                SessionCreateParams.LineItem.PriceData.builder()
+                                                        .setCurrency("usd")
+                                                        .setUnitAmount(amountInCents)
+                                                        .setProductData(
+                                                                SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                                                                        .setName("Order Deposit")
+                                                                        .build()
+                                                        )
+                                                        .build()
+                                        )
+                                        .build()
+                        )
+                        .build();
+
+        Session session = Session.create(params);
+
+        return session.getUrl();
+    }
 }
