9bcca8886ecf51679e5283bc2becf65fc60e7487
Chris-Merced <christopher.r.merced@gmail.com>
2026-05-12 16:37:50 -0500

    Style: Modify front end styling to be more cohesive
    Change "cakes" to "my cakes" in header link
    Change panama city to southport in address above google maps
    Modify background colors for divs to alternate per section

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -9,7 +9,7 @@ export default function Header(): ReactElement {
                 Confection <span>Co</span>
             </Link>
             <nav className="header-nav">
-                <Link to="/cakes" className="header-nav-link">Cakes</Link>
+                <Link to="/cakes" className="header-nav-link">My Cakes</Link>
                 <Link to="/my-bakes" className="header-nav-link">My Bakes</Link>
                 <a href="#policies" className="header-nav-link">Policies</a>
                 <a href="#order" className="header-nav-cta">Order Now</a>
