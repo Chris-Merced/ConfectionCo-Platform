fc8251537da765052ac3128333ff4760b2400e0d
Chris-Merced <christopher.r.merced@gmail.com>
2026-05-12 15:54:11 -0500

    Style: Create cake route to show off cake gallery

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,13 +1,15 @@
 import type { ReactElement } from "react";
+import { Link } from "react-router-dom";
 import "../styles.css";
 
 export default function Header(): ReactElement {
     return (
         <header className="header">
-            <span className="header-brand">
+            <Link to="/" className="header-brand">
                 Confection <span>Co</span>
-            </span>
+            </Link>
             <nav className="header-nav">
+                <Link to="/cakes" className="header-nav-link">Cakes</Link>
                 <a href="#policies" className="header-nav-link">Policies</a>
                 <a href="#order" className="header-nav-cta">Order Now</a>
             </nav>
