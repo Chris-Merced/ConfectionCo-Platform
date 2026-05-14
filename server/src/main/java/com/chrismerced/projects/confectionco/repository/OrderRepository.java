0b0e750da3b83f867c900ec0fd7f701472059030
Chris-Merced <christopher.r.merced@gmail.com>
2026-05-14 14:09:33 -0500

    Feat: Preserve all refunded and deleted order information
    Delete all images on delete and refund order
    Create collapsable grouping of refunded orders on admin page

--- server/src/main/java/com/chrismerced/projects/confectionco/repository/OrderRepository.java
+++ server/src/main/java/com/chrismerced/projects/confectionco/repository/OrderRepository.java
@@ -1,5 +1,6 @@
 package com.chrismerced.projects.confectionco.repository;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,7 +11,7 @@ import com.chrismerced.projects.confectionco.model.OrderStatus;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    List<Order> findByStatusNot(OrderStatus status);
+    List<Order> findByStatusNotIn(Collection<OrderStatus> statuses);
 
     Optional<Order> findByStripeRefundId(String stripeRefundId);
 }
