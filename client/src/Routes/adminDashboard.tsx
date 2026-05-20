0a496c93aaa76e616eba650eda6c48f6550a6250
Chris-Merced <christopher.r.merced@gmail.com>
2026-05-20 12:55:22 -0500

    Fix: Change install directory for heroku in pom.xml

--- client/src/Routes/adminDashboard.tsx
+++ client/src/Routes/adminDashboard.tsx
@@ -19,8 +19,6 @@ const STATUS_SECTIONS: { key: string; label: string; collapsible?: boolean }[] =
 //     the customer to a google/facebook review link
 // Add favicon with company logo
 // Add SEO optimizations if necessary
-// Implement sms_consent change to false on STOP or UNSUBSCRIBE message
-// Implement HELP message for sms
 export default function AdminDashboard(): ReactElement {
     const { isLoading, isAuthenticated, error, loginWithRedirect: login, logout: auth0Logout, user, getAccessTokenSilently } = useAuth0();
 
