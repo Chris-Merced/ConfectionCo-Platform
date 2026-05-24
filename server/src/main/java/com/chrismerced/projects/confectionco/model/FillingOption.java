38594681c719fc51b60f9f55960b41560c7a2528
Chris-Merced <christopher.r.merced@gmail.com>
2026-05-24 17:30:15 -0500

    Feat: create JPA table entities for flavors/buttercreams/fillings

---
+++ server/src/main/java/com/chrismerced/projects/confectionco/model/FillingOption.java
@@ -0,0 +1,22 @@
+package com.chrismerced.projects.confectionco.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "filling_options")
+public class FillingOption {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
